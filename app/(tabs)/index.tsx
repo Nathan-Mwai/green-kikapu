cfd5e45b76ba2f669c9eb3cc2f2a31169be8ba06
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-11-04 12:03:15 +0300

    Add initial homepage header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {FlatList, Pressable, Text, View} from 'react-native'
+import {FlatList, Text, View} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import {offers} from "@/constants";
@@ -23,9 +23,25 @@ const Index = () => {
                 contentContainerClassName='gap-7 px-5 pb-32'
                 ListHeaderComponent={()=>(
                     <View className={'my-5 gap-5'}>
+                        {/*This is the header*/}
+
                         <View className={'flex-between flex-row w-full'}>
                             <View className={'flex-start'}>
-                                <Text className={'small-bold uppercase'}>Search</Text>
+                                <Text className={'small-bold uppercase'}>Welcome User</Text>
+                                <View className={'flex-start flex-row gap-x-1 mt-0.5'}>
+                                    <Text className={'paragraph-semibold text-dark-100'}>Deliver to Nairobi</Text>
+                                </View>
+                            </View>
+
+                            <Text>CartButton</Text>
+                        </View>
+
+                            <View className={'w-full h-[1px] bg-gray-200'} />
+
+                        {/*This is for categories*/}
+                        <View className={'flex-between flex-row w-full'}>
+                            <View className={'flex-start'}>
+                                <Text className={'small-bold uppercase'}>Categories and Search</Text>
                                 <View className={'flex-start flex-row gap-x-1 mt-0.5'}>
                                     <Text className={'paragraph-semibold text-dark-100'}>Find your favorite food</Text>
                                 </View>
