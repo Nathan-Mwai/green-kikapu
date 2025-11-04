e6908dbda7cb9a393a46404ce4f9a67688f2dc64
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-11-04 14:50:38 +0300

    Implement the search UI

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,34 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, TextInput, TouchableOpacity, Image} from 'react-native'
+import React, {useState} from 'react'
+import {useDebouncedCallback} from "use-debounce";
+import {router} from "expo-router";
 
 const SearchBar = () => {
+    const [query, setQuery] = useState()
+
+
+    const handleSearch = (text: string) => {
+        setQuery(text)
+    }
+
     return (
-        <View>
-            <Text>SearchBar</Text>
+        <View className={'searchbar'}>
+            <TextInput
+                className={'flex-1 p-5'}
+                placeholder={'Search for vegetables....'}
+                value={query}
+                onChangeText={handleSearch}
+                // onSubmitEditing={handleSubmit}
+                placeholderTextColor={'#A0A0A0'}
+                returnKeyType={"search"}
+            />
+            <TouchableOpacity
+                className={'pr-5'}
+                onPress={()=> console.log("Oh no stop...")}
+            >
+                <Text>Click</Text>
+                {/*<Image className="size-6" resizeMode="contain" tintColor="#5D5F6D"/> Images to come from assets */}
+            </TouchableOpacity>
         </View>
     )
 }
