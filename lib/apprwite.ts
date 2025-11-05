1845ae457261568c0a84ad11580c07313a9d4c91
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-11-05 08:34:17 +0300

    update lib appwrite naming

--- lib/apprwite.ts
+++
@@ -1,5 +0,0 @@
-export const apprwiteConfig ={
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    platform: 'com.nathan.greenkikapu',
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
